50d4f2b3591e61849af126e78f93cbe2e66b8dcd
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-12-04 20:51:33 +0100

    Implemented the autheticate route

--- app.js
+++ app.js
@@ -39,6 +39,37 @@ module.exports = function serverHandler(req, res) {
         res.end("User created successfully!");
       });
     });
+  } else if (routing === "/authenticateuser" && req.method === "POST") {
+    let body = [];
+    req.on("data", (ch) => {
+      body.push(ch);
+    });
+    req.on("end", () => {
+      const parsedLoginDetails = JSON.parse(Buffer.concat(body).toString());
+
+      fs.readFile(usersFilePath, "utf8", (err, data) => {
+        if (err) {
+          console.log(err.message);
+          res.writeHead(500);
+          res.end("A terrible incident occured. It's your fault!");
+          return;
+        }
+        const allUsers = JSON.parse(data);
+        const { username, password } = parsedLoginDetails;
+        const existingUserIndex = allUsers.findIndex(
+          (user) => user.username === username && user.password === password
+        );
+        if (existingUserIndex === -1) {
+          res.writeHead(404);
+          res.end("Username or password is incorrect. Reconfirm details!");
+          return;
+        }
+        res.writeHead(200);
+        res.end("Login successful!");
+      });
+    });
+  } else if (routing === "/getusers" && req.method === "GET") {
+    res.end("We are here ooo!");
   } else {
     res.writeHead(404);
     res.end("Reconfirm the endpoint and reattempt!");
